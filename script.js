4ab78ec9d06da9373c96d439e4990ec501da8a72
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-08-08 17:45:41 -0300

    :star:
    Mudei o input text para um input range e acrescentei mais algumas coisas.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+let inputRange_size= document.querySelector("#size_senha_slider");
+let size_senha = document.querySelector("#size");
+size_senha.innerHTML = inputRange_size.value;
+
+size_senha_slider.oninput = function() {
+  size_senha.innerHTML = this.value;
+}
+
+let container_senhaGerada = document.querySelector("#senhaGerada");
+
 function gerarCodigoAleatorio() {
-    //pega do input o tamanho da senha
-    let tamanhoSenha = document.getElementById("tamanho").value;
-    //base de caracteres, simbulos e números usado na senha
-    let caracteres = '@#$%&*()}{][+=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let codigo = '';
-    //constroi a senha com base no tamanho
-    for (let i = 0; i < tamanhoSenha; i++) {
-      let indice = Math.floor(Math.random() * caracteres.length);
-      codigo += caracteres.charAt(indice);
-    }
-    document.getElementById("senhaGerada").innerHTML=codigo
+  let tamanhoSenha = document.getElementById("size_senha_slider").value;
+  let caracteres = '@#$%&*()}{][+=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let codigo = '';
+  for (let i = 0; i < tamanhoSenha; i++) {
+    let indice = Math.floor(Math.random() * caracteres.length);
+    codigo += caracteres.charAt(indice);
+  }
+  document.getElementById("senhaGerada").innerHTML=codigo;
+  container_senhaGerada.classList.remove("hide");
 }
